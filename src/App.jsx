ead11b327904e81266362e3d94c5d8ea89582c31
Ali_Shair <alishair7071@gmail.com>
2025-03-02 19:52:22 +0500

    just installed the Taliwind and made a first template

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='font-bold text-red-600'>
+        sdfkjndsfnskj
+      </div>
+      
+    </>
+  )
+}
+
+export default App
