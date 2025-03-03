1e9c55b28569cfd26951a0370a6a17bbfd6e4cb0
Ali_Shair <alishair7071@gmail.com>
2025-03-03 16:15:33 +0500

    create header cpmonent

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return <BrowserRouter>
+
+      <Header/>
   <Routes>
     <Route path='/' element= {<Home/>} />
     <Route path='/sign-in' element= {<SignIn/>} />
