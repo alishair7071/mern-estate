b7dfc832841b3710773a37b85317f88fab6c98c6
Ali_Shair <alishair7071@gmail.com>
2025-03-03 21:43:16 +0500

    connect to mongoDB databse

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
-const express= require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("connected");
+  } catch (e) {
+    console.log(e);
+  }
+};
+connectDb();
 
 const app = express();
 
-
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000!!!");
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!!!");
+});
