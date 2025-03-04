721e4e1425e67919fa4ca69b421fde9610351bff
Ali_Shair <alishair7071@gmail.com>
2025-03-04 16:17:29 +0500

    create the middleware and error handler

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,7 +1,8 @@
 const userModel = require("../models/user.model");
 const bcryptjs = require("bcryptjs");
+const errorHandler = require("../utills/error");
 
-const signUp = async (req, res) => {
+const signUp = async (req, res, next) => {
   console.log(req.body);
   const { userName, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 12);
@@ -11,10 +12,11 @@ const signUp = async (req, res) => {
       email: email,
       password: hashedPassword,
     });
-
     res.status(201).json(newUser);
   } catch (e) {
-    res.status(500).json(e.message);
+      next(e)
+    //here we can use our custom errorHandler function which is in the error.js file
+    //next(errorHandler(550, "error from the function"));
   }
 };
 
--- api/index.js
+++ api/index.js
@@ -21,6 +21,17 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000!!!");
 });
 
-
 app.use('/user', userRouter);
 app.use('/auth', signUpRouter);
+
+
+app.use((err, req, res, next)=>{
+    const statusCode= err.statusCode || 500;
+    const message= err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+            success: false,
+            statusCode: statusCode,
+            message: message,
+    });
+
+});
