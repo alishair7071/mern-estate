f39692edfad7037976ad3d29505909d606a3b862
Ali_Shair <alishair7071@gmail.com>
2025-03-05 05:48:29 +0500

    made sign up page functional

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -4,15 +4,19 @@ const errorHandler = require("../utills/error");
 
 const signUp = async (req, res, next) => {
   console.log(req.body);
+  try {
   const { userName, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 12);
-  try {
-    let newUser = await userModel.create({
+      
+  let newUser = await userModel.create({
       userName: userName,
       email: email,
       password: hashedPassword,
     });
-    res.status(201).json(newUser);
+    res.status(201).json(
+      {user:newUser,
+        message: "user created Successfully"
+      });
   } catch (e) {
       next(e)
     //here we can use our custom errorHandler function which is in the error.js file
--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use('/auth', signUpRouter);
 
 app.use((err, req, res, next)=>{
     const statusCode= err.statusCode || 500;
-    const message= err.message || "Internal Server Error";
+    const message= err.message || "Internal Server Error rewrwer";
     return res.status(statusCode).json({
             success: false,
             statusCode: statusCode,
