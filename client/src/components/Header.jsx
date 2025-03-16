bdad25e53bf4a187bc27e1cf45266d605c244e3b
Ali_Shair <alishair7071@gmail.com>
2025-03-16 19:54:59 +0500

    update header search form functionality to go on url that is typed in seach input

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Header = () => {
 
   const { currentUser }= useSelector((store)=>store.user);
+  const [searchTerm, setSearchTerm]= useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e)=>{
+    e.preventDefault(); 
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery= urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+
+
+  useEffect(()=>{
+    const urlParams= new URLSearchParams(window.location.search);
+    const searchTermFromUrl= urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl);
+    }
+
+  }, [location.search]);
 
   return (
     <header className="bg-slate-200 shadow-md">
@@ -17,13 +37,17 @@ const Header = () => {
           </h1>
         </Link>
 
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center w-24 sm:w-64">
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center w-24 sm:w-64">
           <input
             className=" focus:outline-none"
             type="text"
+            value={searchTerm}
             placeholder="Search..."
+            onChange={(e)=>setSearchTerm(e.target.value)}
           />
+          <button type="submit"> 
           <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <ul className="flex gap-6">
             <Link to="/">
