225d2137d4087d2672d6034787134622893599f3
Ali_Shair <alishair7071@gmail.com>
2025-03-22 22:49:11 +0500

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const userRouter = require('./routes/user.router');
 const authRouter = require('./routes/auth.route');
 const cookieParser = require('cookie-parser'); 
 const listingRouter= require('./routes/listing.route');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -22,6 +23,8 @@ const connectDb = async () => {
 };
 connectDb();
 
+const __dirname = path.resolve();
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!!");
 });
@@ -30,6 +33,11 @@ app.use('/user', userRouter);
 app.use('/auth', authRouter);
 app.use('/listing', listingRouter)
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next)=>{
     const statusCode= err.statusCode || 500;
